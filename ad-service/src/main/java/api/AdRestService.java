60f31a4f7a044be61b6bc02f2452b1a4e528ebde
CorentinBT <corentin.bugnot@etu.unige.ch>
2019-04-02 17:00:42 +0200

    Docker implementation for the ad-service

--- ad-service/src/main/java/api/AdRestService.java
+++ ad-service/src/main/java/api/AdRestService.java
@@ -2,18 +2,18 @@ package api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 @Path("/ad")
 public class AdRestService {
 	
 	@GET
 	@Path("create")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String createAd(@PathParam("title") String name) {
-		return "Ad created with title : " + name;
+	@Produces("text/plain")
+	public Response create(@QueryParam("title") String title) {
+		return Response.ok("Ad with title : " + title + " created.").build();
 	}
 
 }
