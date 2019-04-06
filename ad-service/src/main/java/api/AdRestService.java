b0632d69f32af36442a3bed28ec67d27940e3609
razureladrien <adrien.razurel@etu.unige.ch>
2019-04-06 18:57:20 +0200

    ctrl z

--- ad-service/src/main/java/api/AdRestService.java
+++ ad-service/src/main/java/api/AdRestService.java
@@ -1,59 +1,13 @@
 package api;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-import domain.model.Ad;
-import domain.service.AdService;
-
 @Path("/ad")
 public class AdRestService {
-	
-	
-	@Inject
-	private AdService adservice;
-	
-	
-	public void setPersonservice(AdService as) {
-		adservice = as;
-	}
-
-	
-	@GET
-	@Path("/new/{title}/{description}/{price}")
-	@Produces("text/plain")
-	public Response addNewAd(@PathParam("title") String title, 
-			@PathParam("description") String description, @PathParam("price") double price) {
-		Ad a = new Ad(title, description, price);
-		if(adservice.createAd(a)) {
-			return Response.ok("You inserted "+ a.toString()).build();	
-		} else {
-			return Response.ok("Error").build();	
-		}
-
-
-		
-	}
-	
-	
-
-	@GET
-	@Path("/")
-	@Produces("text/plain")
-	public String getAll() {
-		ArrayList<Ad> all = adservice.getAll();
-		return all.stream().map(p -> p.toString()).collect(Collectors.joining("\n"));
-	}
-	
-	
 	
 	@GET
 	@Path("create")
