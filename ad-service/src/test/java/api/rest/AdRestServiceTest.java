714b8611b9ea1ca3841b3589a716114f79db7a71
bigotromain <romain.bigot@etu.unige.ch>
2019-05-16 17:18:35 +0200

    testing AdRestService

---
+++ ad-service/src/test/java/api/rest/AdRestServiceTest.java
@@ -0,0 +1,35 @@
+package api.rest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import domain.model.Ad;
+import domain.service.AdServiceImpl;
+
+// For integration testing
+//import static io.restassured.RestAssured.*;
+//import static io.restassured.matcher.RestAssuredMatchers.*;
+//import static org.hamcrest.Matchers.*;
+// import static io.restassured.module.jsv.JsonSchemaValidator.*;
+
+
+public class AdRestServiceTest {
+	
+	@InjectMocks
+	private AdRestService adRestService;
+	
+	@Test
+	public void getAdTest() {
+		Ad ad = adRestService.get((long) 1);
+		assertNotNull(ad);
+		assertEquals(ad.getId(), (long) 1);
+	}
+	
+	
+	
+		
+}
+
