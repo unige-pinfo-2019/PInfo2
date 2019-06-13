0194a6c0780d4c6205236e6ae027635bea3feca0
CorentinBT <corentin.bugnot@etu.unige.ch>
2019-06-13 11:34:40 +0200

    Web ui : typos which prevented the correct print of user informations

--- web-ui/src/app/profile/profile.component.ts
+++ web-ui/src/app/profile/profile.component.ts
@@ -22,6 +22,7 @@ export class ProfileComponent implements OnInit {
         this.route.data.pipe(
             concatMap(data => {
                 this.profile = data.profile;
+                this.profile.mobile = data.profile.attributes.mobile;
                 // Load the current user's data.
                 return this.userService.currentUser.pipe(tap(
                     (userData: User) => {
